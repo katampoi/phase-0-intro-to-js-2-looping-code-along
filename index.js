1f7ce61fbf91b07bd609618473748b6c509d0889
katampoi <simonkatampoi@student.moringaschool.com>
2022-06-03 15:10:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const name = ['simon', 'Katampoi', 'mbatiany'];
+const event = 'surprise';
+
+function writeCards(name, event) {
+    let newArray = [];
+    for (let i = 0; i < name.length; i++) {
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        
+    }
+
+    return newArray;
+}
+
+function countDown(count = 10) {
+    while (count >= 0) {
+       console.log(count--);
+   }
+}
